bb7eb788fa95633c72ce0083d7a2b236308f4bdb
daveg <davegalea27@gmail.com>
2025-10-17 13:34:35 +0200

    Added WinAPI and WPF Weather Projects

---
+++ WPFWeather.App/Windows/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using WPFWeather.App.Services;
+using WPFWeather.App.ViewModels;
+
+namespace WPFWeather.App.Windows
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new WeatherViewModel(new WeatherDiskService());
+        }
+    }
+}
